59dd5440d9fd4509d67ba826d27d7516887095dc
Ian Dela Cruz <iandc76@gmail.com>
2020-05-18 18:46:37 +0800

    Add initial codes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import crypto from 'crypto';
 
-const myRandom = () => {
-  return crypto.randomBytes(16).toString('hex');
+export const isValidPassword = (inputPassword, salt, hash) => {
+  const inputHash = crypto
+    .pbkdf2Sync(inputPassword, salt, 1000, 64, `sha512`)
+    .toString(`hex`);
+  return inputHash === hash;
 };
 
-export default myRandom;
+export const getHashSalt = (pw) => {
+  // Creating a unique salt for a particular user
+  const salt = crypto.randomBytes(16).toString('hex');
+
+  // Hashing user's salt and password with 1000 iterations,
+  // 64 length and sha512 digest
+  const hash = crypto.pbkdf2Sync(pw, salt, 1000, 64, `sha512`).toString(`hex`);
+
+  return { hash, salt };
+};
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,8 +1,41 @@
-import myRandom from '../src';
+import { getHashSalt, isValidPassword } from '../src';
 
-describe('My Random Test', () => {
-  it('should be 32 characters', () => {
-    const rand = myRandom();
-    expect(rand.length).toEqual(32);
+describe('Password', () => {
+  [
+    {
+      description: 'should generate password and validate it',
+      password: 'hello world',
+      inputPw: 'hello world',
+      expected: true,
+    },
+    {
+      description: 'should not validate',
+      password: 'hello world',
+      inputPw: 'world hello',
+      expected: false,
+    },
+    {
+      description: 'should not validate',
+      password: 'hello world',
+      inputPw: 'helloworld',
+      expected: false,
+    },
+    {
+      description: 'should not validate',
+      password: 'hello world',
+      inputPw: 'hello worl',
+      expected: false,
+    },
+    {
+      description: 'should not validate',
+      password: 'hello world',
+      inputPw: 'ello world',
+      expected: false,
+    },
+  ].forEach(({ description, password, inputPw, expected }) => {
+    it(description, () => {
+      const { hash, salt } = getHashSalt(password);
+      expect(isValidPassword(inputPw, salt, hash)).toBe(expected);
+    });
   });
 });
